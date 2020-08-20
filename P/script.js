baf81a9fc1c43dba3d4177460cbc08ce1faf81f2
SaiVikasP <123003181@sastra.ac.in>
2020-08-20 14:14:28 +0530

    Message Board.

--- P/script.js
+++ P/script.js
@@ -1,13 +1,18 @@
 function sayHai() {
+	this.textContent="Said it!"
 	var name =
 		document.getElementById("name").value;
-		var message="<h2>Hai "+name+"!</h2>";
+		var message="<h2>Hai "+name+"!</h2>"+" Click on message board for new message";
 	document
 		.getElementById("output")
 		.innerHTML=message;
 }
 
+document.querySelector('button')
+	.addEventListener("click",sayHai);
+
 function showLink() {
+	this.textContent="Click on the Course!"
 	var link=
 		"<div>Welcome to</div>"+
 		"<div>Sastra deemed university on Coursera</div>"+
@@ -16,3 +21,5 @@ function showLink() {
 		.getElementById("output2")
 		.innerHTML=link;
 }
+document.querySelector('button > a')
+	.addEventListener("click",showLink);
